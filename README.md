ddfbe14744cc2ce475b5f5eed60c6240e4396657
Steven Moore <steven.ian.moore@gmail.com>
2017-11-10 21:12:04 +1100

    Add poisson's equation.

--- README.md
+++ README.md
@@ -1,56 +1,28 @@
-# timoshenko-fem
+# The Finite Element Method: Examples with a Timoshenko Beam and Poisson's Equation
 
-This project is a small example of coding the finite element method. The coded
-example is to determine the modal frequencies of a Timoshenko cantilever.
+This project is a small example of coding the finite element method. The first 
+coded example is to determine the modal frequencies of a Timoshenko cantilever.
+The second solves Poisson't equation which models many physical phenomena.
 
-<!--
-The kinematics of a Timoshenko beam are:
 
-$$ u_1(x,y,z) = y\theta(x), \qquad u_3(x,y,z) = w(x). $$
+## Poisson's Equation
 
-With these kinematric, the non-zero strains are:
+Poisson's equation results from modeling in many physcial domains including
+heat conduction and electrostatics. The example coded in `poisson_fem.py` is 
+used to determine the temperature field `T(x,y)` resulting from a set of 
+heat sources `Q(x,y)` on a rectangular structure with varying heat conductivity
+`k(x,y)`.
 
-$$ 
-    S_1 &= y\frac{\partial\theta}{\partial x},
-    \qquad
-    S_6 &= \theta + \frac{\partial w}{\partial x}.
-$$
-
-With these non-zero strains, the constitutive equation of the linear material 
-become:
-
-$$ T_1 = ES_1 \qquad T_6 = \kappa G S_6, $$
-    
-where $E$ is Young's modulus, $\kappa$ is a correction factor that reduces the 
-shear stress in the finite element model, and $G$ is the shear modulus. The
-shear modulus is a function of Young's modulus and Poisson's ratio:
-
-$$ G = \frac{E}{2(1+\nu)}. $$
-   
-To develop the finite element equations, the strain and kinetic energy of the 
-beam are derived and Hamilton's principle is applied. The strain energy is:
-
-$$ U = \frac{1}{2}\int_V T_1S_1 + T_6S_6 \; dV $$
-
-where I is the moment of inertia of the beam and A is the cross sectional area:
- 
-$$ I = \frac{t_zt_y^3}{12}, \qquad A = t_zt_y.$$
-
-The shape functions are given by:
-
-$$
-    \begin{bmatrix} w \\ \theta_z \end{bmatrix}
-$$
-
-The element and mass matrices are:
-Hamilton's principle must hold system described. The kinetic energy and strain energy 
-$$
-\begin{align}
-    K_e = \int_{-1}^{1}EI\left(\frac{1}{a}\frac{dN}{d\xi})^T
-        \left(\frac{1}{a}\frac{dN}{d\xi})
-$$
--->
+Three different approaches to assembling are explored. By far the worst is
+creating a block diagonal sparse matrix and then applying constraints. The
+standard iterative method has good performance. However, an approach building
+the contents of the sparse matrix using vector operations was the fastest.
 
 ## References
 
 Matlab Codes for Finite Element Analysis; A. J. M. Ferreira; 2009; Springer
+
+Effecient topology optimization in MATLAB using 88 lines of code;
+    Erik Andreassen, Anders Clausen, Mattias Schevenels, Boyan S. Lazarov,
+    Ole Sigmund; Structural and Multidisciplinary Optimization; January 
+    2011; Volume 43, Issue 1, pp 1–16;
---
+++ poisson_fem.py
@@ -0,0 +1,218 @@
+import time
+import numpy as np
+import scipy.sparse as sparse
+
+
+class Node(object):
+    
+    def __init__(self, i, j, dof):
+        """
+        :i: is the x-index of the node.
+        :j: is the y-index of the node.
+        :dof: is the dof index associated with the node.
+        """
+        self.i = i
+        self.j = j
+        self.dof = dof
+        
+        
+    def __repr__(self):
+        return 'Node %d: %d, %d' % (self.dof, self.i, self.j)
+        
+        
+class Element(object):
+    
+    def __init__(self, i, j, nodes_2D):
+        """
+        :i: is the x-index of the element.
+        :j: is the y-index of the element.
+        :nodes_2D: list of nodes indexed by (i,j).
+        """
+        self.i = i
+        self.j = j
+        self.n1 = nodes_2D[i][j]
+        self.n2 = nodes_2D[i + 1][j]
+        self.n3 = nodes_2D[i + 1][j + 1]
+        self.n4 = nodes_2D[i][j + 1]
+        self.dofs = np.array([self.n1.dof, self.n2.dof, self.n3.dof, self.n4.dof])
+        
+        
+class RectangularMesh(object):
+    
+    def __init__(self, nx, ny):
+        """
+        :nx: the number of elements in the x-direction.
+        :ny: the number of elements in the y-direction.
+        """
+        # Create the nodes.
+        node_index = np.ndindex(nx + 1, ny + 1)
+        self.nodes = [Node(i, j, k) for k, (i, j) in enumerate(node_index)]
+        self.nodes_2D = [[None for j in range(ny + 1)] for i in range(nx + 1)]
+        for i, n in enumerate(self.nodes):
+            self.nodes_2D[n.i][n.j] = n
+            
+        # Create the elements.
+        elem_index = np.ndindex(nx, ny)
+        self.elements = [Element(i, j, self.nodes_2D) for i, j in elem_index]
+        self.elements_2D = [[None for j in range(ny)] for i in range(nx)]
+        for e in self.elements:
+            self.elements_2D[e.i][e.j] = e
+            
+        # Number of nodes and elements in the mesh.
+        self.n_elem = len(self.elements)
+        self.n_node = len(self.nodes)
+        
+        # Node constraint matrix used by the block diagonal assembler.
+        row = np.arange(4 * self.n_elem)
+        col = np.empty(4 * self.n_elem)
+        for i, e in enumerate(self.elements):
+            col[4*i:4*i+4] = e.dofs
+        val = np.ones(4 * self.n_elem)
+        shape = (4 * self.n_elem, len(self.nodes))
+        self.con = sparse.coo_matrix((val, (row, col)), shape=shape).tocsr()
+        
+        # All, free, and boundary dofs. Boundary nodes are at j == 0.
+        self.all_dofs = np.arange(self.n_node)
+        self.fixed_dofs = [n.dof for n in self.nodes if n.j == 0]
+        self.free_dofs = np.setdiff1d(self.all_dofs, self.fixed_dofs)
+        
+        
+class ElementModel(object):
+    
+    def __init__(self, a, b):
+        """
+        :a: half the width of an element in the x-direction.
+        :b: half the width of an element in the y-direction.
+        """
+        points = np.array([[-1, -1], [1, -1], [1, 1], [-1, 1]]) / np.sqrt(3)
+        jacobian = a*b
+        ke = np.zeros((4, 4))
+        fe = np.zeros((4, 1))
+        for p in points:
+            n, dndxi, dndeta = self.shapes(p)
+            ke += jacobian * (dndxi.T @ dndxi / a*a + dndeta.T @ dndeta / b*b)
+            fe += jacobian * n.T
+        self.ke = ke
+        self.fe = fe
+        
+            
+    def shapes(self, point):
+        """
+        :point: the point (xi, eta) to evaluate the shape functions.
+        """
+        xi, eta = point
+        xs = np.array([[-1, 1, 1, -1]])
+        es = np.array([[-1, -1, 1, 1]])
+        n = 0.25 * (1 + xs * xi) * (1 + es * eta)
+        dndxi = xs * 0.25 * (1 + es * eta)
+        dndeta = es * 0.25 * (1 + xs * xi) 
+        return n, dndxi, dndeta
+    
+    
+class AssemblerBlock(object):
+    
+    def __init__(self, mesh, model, k, q):  
+    
+        t0 = time.time()
+        k_block = [ke * model.ke for ke in k]
+        f_block = [qe * model.fe for qe in q]
+        self.sysk1 = mesh.con.T @ sparse.block_diag(k_block, format='csr') @ mesh.con
+        self.sysf1 = mesh.con.T @ sparse.coo_matrix(np.vstack(f_block)).tocsr()
+        t1 = time.time()
+        print('Block Assembley: %g (s)' % (t1-t0))
+        
+        
+    def get_system(self):
+        return self.sysk1, self.sysf1
+
+
+class AssemblerIterative(object):
+    
+    def __init__(self, mesh, model, k, q):
+
+        t0 = time.time()
+        ntriplet = 0
+        fntriplet = 0
+        row = np.zeros(mesh.n_elem * 16)
+        col = np.zeros(mesh.n_elem * 16)
+        val = np.zeros(mesh.n_elem * 16)
+        frow = np.zeros(mesh.n_elem * 4)
+        fcol = np.zeros(mesh.n_elem * 4)
+        fval = np.zeros(mesh.n_elem * 4)
+        for qe, ke, e in zip(q, k, mesh.elements):
+            dof = [e.n1.dof, e.n2.dof, e.n3.dof, e.n4.dof]
+            for ii, jj in np.ndindex(4, 4):
+                row[ntriplet] = dof[ii]
+                col[ntriplet] = dof[jj]
+                val[ntriplet] = ke * model.ke[ii, jj]
+                ntriplet += 1
+            for ii in range(4):
+                frow[fntriplet] = dof[ii]
+                fcol[fntriplet] = 0
+                fval[fntriplet] = qe * model.fe[ii]
+                fntriplet += 1
+                
+        shape = (mesh.n_node, mesh.n_node)
+        fshape = (mesh.n_node, 1)
+        self.sysk2 = sparse.coo_matrix((val, (row, col)), shape=shape).tocsr()
+        self.sysf2 = sparse.coo_matrix((fval, (frow, fcol)), shape=fshape).tocsr()
+        t1 = time.time()
+        print('Iterative Assembley: %g (s)' % (t1-t0))
+
+
+    def get_system(self):
+        return self.sysk2, self.sysf2
+
+
+class AssemblerVector(object):
+    """This approach follows the work in the article:
+    Effecient topology optimization in MATLAB using 88 lines of code;
+    Erik Andreassen, Anders Clausen, Mattias Schevenels, Boyan S. Lazarov,
+    Ole Sigmund; Structural and Multidisciplinary Optimization; January 
+    2011; Volume 43, Issue 1, pp 1–16;
+    """
+    def __init__(self, mesh, model, k, q):
+    
+        t0 = time.time()
+        kr = np.hstack([np.hstack([e.dofs for _ in range(4)]) for e in mesh.elements])
+        kc = np.hstack([np.hstack([d*np.ones(4) for d in e.dofs]) for e in mesh.elements])
+        fr = np.hstack([e.dofs for e in mesh.elements])
+        fc = np.zeros_like(fr)
+        kv = np.expand_dims(k, axis=1) @ np.expand_dims(model.ke.ravel(), axis=0)
+        kv = np.ravel(kv)
+        fv = np.expand_dims(q, axis=1) @ model.fe.T 
+        fv = np.ravel(fv)
+        
+        shape = (mesh.n_node, mesh.n_node)
+        fshape = (mesh.n_node, 1)     
+        self.sysk3 = sparse.coo_matrix((kv, (kr, kc)), shape=shape).tocsr()
+        self.sysf3 = sparse.coo_matrix((fv, (fr, fc)), shape=fshape).tocsr()
+        t1 = time.time()
+        print('Vector Assembley: %g (s)' % (t1-t0))
+        
+        
+    def get_system(self):
+        return self.sysk3, self.sysf3
+    
+
+if __name__ == '__main__':
+    
+    nelx = 20
+    nely = 20
+    a = 0.1
+    b = 0.1
+    
+    mesh = RectangularMesh(nelx, nely)
+    model = ElementModel(a, b)
+    
+    # q is the sources, k is the conduction.
+    q = np.zeros(mesh.n_elem)
+    q[7] = 1
+    k = np.arange(mesh.n_elem) + 1
+       
+    assembler = AssemblerVector(mesh, model, k, q)
+    sysk, sysf = assembler.get_system()
+    sysk = sysk[mesh.free_dofs, :][:, mesh.free_dofs]
+    sysf = sysf[mesh.free_dofs]
+    u = sparse.linalg.spsolve(sysk, sysf)
+        
